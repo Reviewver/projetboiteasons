c841e7c7c48750815d769e060326132176058d40
Simon Hemery <simon.hemery.56350@gmail.com>
2021-02-19 00:11:00 +0100

    Suppression de l'ancienne fonction de filtrage et ajout d'une nouvelle qui ajoute le périphérique détecter dans un buffer si celui ci est une balise

--- BoiteASons/BoiteASons.ino
+++ BoiteASons/BoiteASons.ino
@@ -8,12 +8,98 @@
 #include <BLEScan.h>
 #include <BLEAdvertisedDevice.h>
 
+// Définition de la structure Balise
+struct balise
+{
+  // Numéro de la balise
+  int number;
+
+  // Adresse mac de la balise
+  std::string mac;
+
+  // Activer si la balise n'a pas été trouvée
+  bool notFound;
+};
+
+// Nombre de balise dans le tableau
+const int balise_max = 2;
+// Tableau des adresses mac considéré comme étant des balises
+const struct balise baliseList[balise_max] = {{1,"d5:f4:0d:a3:66:e4"},{2,"d7:02:10:a7:5d:e0"}};
+
+// Nombre de balise détecter maximum
+const int balise_max_detect = 2;
+// Tableau des balise détecter
+struct balise detect_balise_buffer[balise_max_detect] = {};
+
 int scanTime = 5; //In seconds
 BLEScan* pBLEScan;
+
+// Vérifie que c'est une balise et retourne le numéro de la balise si elle est détecter sinon retourne -1
+struct balise check_if_balise_and_return_number(BLEAdvertisedDevice device)
+{
+    for(int i = 0 ; i < balise_max ; i++)
+    {
+      // Si l'address mac de la balise est présent dans la liste
+      if(device.getAddress().toString() == baliseList[i].mac)
+      {
+        return baliseList[i];        
+      }
+    }
+    struct balise balise;
+    balise.notFound = true;
+
+    return balise;
+}
+
+// Vérifie que la balise n'est pas déjà présente dans le tableau des détection
+bool balise_is_detect_array(struct balise detect_balise)
+{
+  for(int i = 0 ; i < balise_max_detect ; i++)
+  {
+      if((detect_balise.number == detect_balise_buffer[i].number) && (detect_balise.mac == detect_balise_buffer[i].mac))
+      {
+        return true;
+      }
+  }
+
+  return false;
+}
+
+// Ajoute la balise dans le tableau des balise détecter
+bool add_balise_detect_buffer(struct balise detect_balise)
+{
+    for(int i = 0 ; i < balise_max_detect ; i++)
+    {
+      if(detect_balise_buffer[i].number==0)
+      {
+        detect_balise_buffer[i] = detect_balise;
+        return true;
+      }
+    }
+    return false;
+}
 
 class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
     void onResult(BLEAdvertisedDevice advertisedDevice) {
       Serial.printf("Périphérique : %s \n", advertisedDevice.toString().c_str());
+      struct balise baliseDetect = check_if_balise_and_return_number(advertisedDevice);
+      if(!baliseDetect.notFound)
+      {
+        Serial.printf("Balise détécter numéro => %d", baliseDetect.number);
+        if(!balise_is_detect_array(baliseDetect))
+        {    
+          Serial.printf("La balise n'est pas dans le tableau");
+          if(add_balise_detect_buffer(baliseDetect))
+          {
+            Serial.printf("La balise à été ajoutée avec succès");
+          }
+          else
+          {
+            Serial.printf("Le buffer de détection est plein");
+          }
+        }
+      }
+      
     }
 };
 
@@ -34,23 +120,10 @@ void setup() {
   pBLEScan->setWindow(99);  // less or equal setInterval value
 }
 
-// Filtrer 
-void filtreBalise(BLEScanResults foundDevices)
-{
-    Serial.print("Nombre de périphérique trouvés : ");
-    Serial.println(foundDevices.getCount());
-    BLEAdvertisedDevice device = foundDevices.getDevice(0);
-    Serial.printf(device.toString().c_str());
-    if(device.toString() == "RDL51822")
-    {
-      Serial.println("Balise");
-    }
-}
 void loop() {
   // Lance le scanner
   BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
   Serial.println("Recherche terminé !");
-  filtreBalise(foundDevices);
   pBLEScan->clearResults();   // delete results fromBLEScan buffer to release memory
   delay(2000);
 }
